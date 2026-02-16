d84e408a7bbcb90091d26467d48928b29084357a
Mithrandir <166934084+Mitrandir00@users.noreply.github.com>
2026-02-16 19:14:22 +0100

    nulla

--- Boba/Assets/Scenes/Andrea/Scripts/BobaRecipe.cs
+++ Boba/Assets/Scenes/Andrea/Scripts/BobaRecipe.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 [CreateAssetMenu(fileName = "BobaRecipe", menuName = "Boba/BobaRecipe")]
 public class BobaRecipe : ScriptableObject
 {
-    public string id;          // es. "classic_milk_tea"
+    public string id;          // es. "classic_milk_tea" ppppp
     public string displayName; // es. "Classic Milk Tea"
     public Sprite icon;        // la useremo dopo per il balloon
     public List<IngredientSpec> ingredients = new();
